6ca8573b3010038a96bacd9cf8378a794035e224
sam oteo <tortillabar@me.com>
2017-03-01 16:49:45 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  #attr_accessor :author, :page_count, :genre
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -8,6 +10,12 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  # create the writer for genre and add the logic for the class contstant
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
   end
 
 end
